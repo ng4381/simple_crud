99808b92efc7641b015969fef35545ace9963b1c
Nikita Goloveshko <ng4381@mail.ru>
2021-12-11 23:54:18 +0300

    Add tests

--- src/test/java/com/noname/simple_crud/controller/UserControllerTest.java
+++ src/test/java/com/noname/simple_crud/controller/UserControllerTest.java
@@ -1,18 +1,21 @@
 package com.noname.simple_crud.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.noname.simple_crud.model.User;
-import com.noname.simple_crud.repository.UserRepository;
+import com.noname.simple_crud.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -22,21 +25,37 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Test
-    public void test() throws Exception {
+    public void getUsersByIdTest() throws Exception {
 
         User user = new User();
         user.setId(1);
         user.setFirstName("test");
-        when(userRepository.findById(1L)).thenReturn(java.util.Optional.of(user));
+        when(userService.getUserById(1L)).thenReturn(user);
 
         mockMvc.perform(get("/users/1"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().json("{\"id\":1,\"firstName\":\"test\",\"lastName\":null,\"email\":null,\"dateOfBirth\":null}"));
+                .andExpect(status().isOk());
 
-        verify(userRepository).findById(1L);
+        verify(userService).getUserById(1L);
+    }
+
+    @Test
+    public void createUserTest() throws Exception {
+
+        User user = new User();
+        user.setId(1);
+        user.setFirstName("test");
+        when(userService.create(any())).thenReturn(user);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON_VALUE).content(json))
+                .andExpect(status().isCreated());
+
+        verify(userService).create(any());
     }
 }
